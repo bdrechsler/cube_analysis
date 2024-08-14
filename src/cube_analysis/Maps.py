255fec68422bf0d3458cd84edb236f8a241746b3
Blake Drechsler <wbd814@gmail.com>
2024-08-13 17:41:53 -0700

    added ratio map, load, and write

--- src/cube_analysis/Maps.py
+++ src/cube_analysis/Maps.py
@@ -1,8 +1,33 @@
 import numpy as np
-
+import pickle
 
 class Maps:
     def __init__(self, line, line_map, cont_map):
         self.line = line
         self.line_map = line_map
         self.cont_map = cont_map
+
+        map_shape = np.shape(line_map)
+        ratio_map = np.zeros(map_shape)
+        
+        for i in range(map_shape[0]):
+            for j in range(map_shape[1]):
+
+                if line_map[i, j] == 0 or cont_map[i, j] == 0:
+                    ratio_map[i, j] = 0
+                elif np.isnan(line_map[i, j]) or np.isnan(cont_map[i, j]):
+                    ratio_map[i, j] = np.nan
+                else:
+                    ratio_map[i, j] = line_map[i, j] / ratio_map[i, j]
+
+        self.ratio_map = ratio_map
+
+    
+    @classmethod
+    def load(cls, fname):
+        with open(fname, "rb") as f:
+            return pickle.load(f)
+        
+    def write(self, fname):
+        with open(fname, "wb") as f:
+            pickle.dump(self, f)
