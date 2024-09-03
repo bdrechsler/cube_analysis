finally fixed line flux calculation

src/cube_analysis/Spectrum.py
@@ -68,8 +68,8 @@ class Spectrum:
             fitter = fitting.LevMarLSQFitter()
             g = fitter(g_init, self.wvl.value, self.flux.value)
             A = g.amplitude * u.Jy
-            sigma = (c / (g.stddev*u.um)).to(u.Hz)
-            line_flux = (A * sigma) * np.sqrt(2*np.pi)
+            sigma = ((g.stddev * u.um) * (c / self.line.wvl**2)).to(u.Hz)
+            line_flux = A * sigma * np.sqrt(2*np.pi)
             self.line_model = g
             self.line_flux = line_flux.to(u.erg/u.s/u.cm**2)
         else: