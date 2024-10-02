43e2b340343beafcf0513814215774ef0cc2c60b
bdrechsler <wbd814@gmail.com>
2024-10-02 16:31:04 -0400

    only define eu if given

--- src/cube_analysis/Line.py
+++ src/cube_analysis/Line.py
@@ -10,25 +10,28 @@ class Line:
         self.chan = self.get_chan(self.wvl.value)
         self.lw = lw * u.um
         self.id = id
-        self.Eu = Eu * u.K
-        self.Aul = Aul * 1e-7 / u.s
+        if Eu:
+            self.Eu = Eu * u.K
+        if Aul:
+            self.Aul = Aul * 1e-7 / u.s
 
     @staticmethod
     def get_chan(wvl):
-        chan_bounds =  {'nirspec': [2.8708948855637573, 5.269494898093398],
-                        'ch1-long': [6.530400209798245, 7.649600181524875],
-                        'ch2-long': [10.010650228883605, 11.699350233480798],
-                        'ch3-long': [15.41124984738417, 17.978749789996073],
-                        'ch4-long': [24.40299961855635, 28.69899965589866],
-                        'ch1-short': [4.900400095357327, 5.739600074157352],
-                        'ch1-medium': [5.6603998474020045, 6.629999822907848],
-                        'ch2-short': [7.5106502288836055, 8.770350232312921],
-                        'ch2-medium': [8.670650076295715, 10.13055008027004],
-                        'ch3-short': [11.551250190706924, 13.47125014779158],
-                        'ch3-medium': [13.341250152559951, 15.568750102771446],
-                        'ch4-short': [17.70300076296553, 20.94900079118088],
-                        'ch4-medium': [20.693000534083694, 24.47900056699291]}
-                        
+        chan_bounds = {
+            "nirspec": [2.8708948855637573, 5.269494898093398],
+            "ch1-long": [6.530400209798245, 7.649600181524875],
+            "ch2-long": [10.010650228883605, 11.699350233480798],
+            "ch3-long": [15.41124984738417, 17.978749789996073],
+            "ch4-long": [24.40299961855635, 28.69899965589866],
+            "ch1-short": [4.900400095357327, 5.739600074157352],
+            "ch1-medium": [5.6603998474020045, 6.629999822907848],
+            "ch2-short": [7.5106502288836055, 8.770350232312921],
+            "ch2-medium": [8.670650076295715, 10.13055008027004],
+            "ch3-short": [11.551250190706924, 13.47125014779158],
+            "ch3-medium": [13.341250152559951, 15.568750102771446],
+            "ch4-short": [17.70300076296553, 20.94900079118088],
+            "ch4-medium": [20.693000534083694, 24.47900056699291],
+        }
 
         line_chan = 0
         for chan, bounds in chan_bounds.items():
@@ -36,18 +39,27 @@ class Line:
                 line_chan = chan
                 break
         return line_chan
-    
+
     @staticmethod
     def load_lines(list_file):
-        t = Table.read(list_file, delimiter=",", format='ascii')
+        t = Table.read(list_file, delimiter=",", format="ascii")
         names = t["line_name"].data
         plot_names = t["plot_name"].data
         wvls = t["wvl"].data
         lws = t["lw"].data
         ids = t["id"].data
 
-        plot_names_str = [r'{}'.format(str(x)) for x in plot_names]
+        plot_names_str = [r"{}".format(str(x)) for x in plot_names]
 
-        line_list = [Line(wvls[i], names[i], ids[i], lws[i], plot_names_str[i]) for i in range(len(names))]
+        line_list = [
+            Line(
+                wvl=wvls[i],
+                name=names[i],
+                id=ids[i],
+                lw=lws[i],
+                plot_name=plot_names_str[i],
+            )
+            for i in range(len(names))
+        ]
         return line_list
-        
+
